e66469c62ece770507890db870fd91560009d28f
paul <paul.mabille@gmail.com>
2020-09-15 09:57:14 +0200

    create profile module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './components/app.component';
 import { AskAppointementComponent } from './components/ask-appointement/ask-appointement.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
+import { ProfileComponent } from './components/profile/profile.component';
 import { ResultsComponent } from './components/results/results.component';
 import { RestService } from './services/rest.service';
 
@@ -14,6 +15,7 @@ import { RestService } from './services/rest.service';
     NavbarComponent,
     ResultsComponent,
     AskAppointementComponent,
+    ProfileComponent,
   ],
   imports: [HttpClientModule, BrowserModule, FormsModule, ReactiveFormsModule],
   providers: [RestService],
