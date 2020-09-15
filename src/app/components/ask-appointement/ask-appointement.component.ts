3c76ee8fafc400f5a85947dc426ee712ac17c129
RayanBarbara <rayan.barbara.1998@gmail.com>
2020-09-15 10:58:53 +0200

    Appointment framework

--- src/app/components/ask-appointement/ask-appointement.component.ts
+++ src/app/components/ask-appointement/ask-appointement.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {FormBuilder} from '@angular/forms';
 
 @Component({
   selector: 'app-ask-appointement',
@@ -10,6 +11,10 @@ export class AskAppointementComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  addAppointment(){
+    alert("Hello world!");
   }
 
 }
