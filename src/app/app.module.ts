6a40a362062c37816acf73232a3b77519d23c7d2
paul <paul.mabille@gmail.com>
2020-09-15 14:13:28 +0200

    get rid of navbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,6 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './components/app.component';
 import { AskAppointementComponent } from './components/ask-appointement/ask-appointement.component';
-import { NavbarComponent } from './components/navbar/navbar.component';
 import { ProfileComponent } from './components/profile/profile.component';
 import { ResultsComponent } from './components/results/results.component';
 import { RestService } from './services/rest.service';
@@ -12,7 +11,6 @@ import { RestService } from './services/rest.service';
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent,
     ResultsComponent,
     AskAppointementComponent,
     ProfileComponent,
