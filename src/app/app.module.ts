e48588a7e7b69722e6e42559b9fea8bc4ccb19a8
paul <paul.mabille@gmail.com>
2020-09-15 09:32:03 +0200

    moove main component and add httpprovider

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,8 @@
+import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
-import { AppComponent } from './app.component';
+import { AppComponent } from './components/app.component';
 import { AskAppointementComponent } from './components/ask-appointement/ask-appointement.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { ResultsComponent } from './components/results/results.component';
@@ -13,7 +15,7 @@ import { RestService } from './services/rest.service';
     ResultsComponent,
     AskAppointementComponent,
   ],
-  imports: [BrowserModule],
+  imports: [HttpClientModule, BrowserModule, FormsModule, ReactiveFormsModule],
   providers: [RestService],
   bootstrap: [AppComponent],
 })
---
+++ src/app/components/app.component.ts
@@ -0,0 +1,25 @@
+import { Component } from '@angular/core';
+import { Patient } from '../../assets/resources/patient';
+import { RestService } from '../services/rest.service';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  patient: Patient;
+
+  constructor(private service: RestService) {
+    service.getPatient().then((patient) => {
+      this.patient = patient;
+      console.log(this.patient);
+    });
+  }
+
+  title = 'adroit';
+  page: string;
+  navigate(page) {
+    this.page = page;
+  }
+}
